1afe2f92f495518c97efab3596adfc82b1b6085d
JBLew <autumnlewjb@gmail.com>
2020-09-15 23:12:03 +0800

    latest successful model

--- writer_tools/text_detection.py
+++ writer_tools/text_detection.py
@@ -3,12 +3,20 @@ import io
 import base64
 import json
 
-def request_body():
+def _request_body(annotate_image_request):
+    total_request = []
+    for request in annotate_image_request:
+        total_request.append(request)
+    
+    return dict(requests=total_request)
+
+
+def make_image_request(filename):
     feature = dict(type='DOCUMENT_TEXT_DETECTION', maxResults="1", model="builtin/stable")
     feature_list = [feature]
     
 
-    with open('resources/english.jpg', 'rb') as input_file:
+    with open(f'resources/{filename}.jpg', 'rb') as input_file:
         content = base64.b64encode(input_file.read()).decode('ascii')
         print(type(content))
     
@@ -16,7 +24,7 @@ def request_body():
 
     annotate_image_request = dict(image=image, features=feature_list)
 
-    return dict(requests=[annotate_image_request])
+    return annotate_image_request
 
 
 def authenticate():
@@ -26,9 +34,9 @@ def authenticate():
     return str(api)
 
 
-def make_request():
+def make_request(annotate_image_request):
     endpoint = 'https://vision.googleapis.com/v1/images:annotate'
-    response = requests.post(endpoint, json=request_body(), params={'key': authenticate()})
+    response = requests.post(endpoint, json=_request_body(annotate_image_request), params={'key': authenticate()})
     
     return response.json()
 
@@ -38,11 +46,30 @@ def record_response(response):
         json.dump(response, json_file)
 
 
-def get_text(response):
-    text = response['responses'][0]['textAnnotations'][0]["description"]
-    return text
+def record_text(text_list):
+    index = 1
+    for text in text_list:
+        with open(f'resources/output/output-{index}.txt', 'w+') as output_file:
+            output_file.write(text)
+        index += 1
+    print('done')
+
+
+def get_text(responses):
+    result = []
+    for response in responses['responses']:
+        text = response['fullTextAnnotation']['text']
+        result.append(text)
+
+    return result
 
 
 if __name__ == '__main__':
-    response = make_request()
-    print(get_text(response))
+    filenames = ['question1', 'question2', 'passage1', 'passage2']
+    annotate_image_requests = []
+    for filename in filenames:
+        annotate_image_requests.append(make_image_request(filename))
+    
+    responses = make_request(annotate_image_requests)
+    text_list = get_text(responses)
+    record_text(text_list)
